40a2039c923364fd6e104f58cffe0fbe62ff5f28
peepatnice1212 <peepatnice@gmail.com>
2024-03-10 23:12:03 +0700

    [Edit] deploy streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-print('streamlit')
+import streamlit as st
+
+def main():
+    st.title('My Streamlit App')
+    st.write('Welcome to my first Streamlit app!')
+
+if __name__ == '__main__':
+    main()
