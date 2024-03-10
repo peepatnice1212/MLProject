e9e0ad7f3d27ae142238db3f6f359db18fda9f0e
peepatnice1212 <peepatnice@gmail.com>
2024-03-10 23:28:22 +0700

    [Edit] test write

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ def main():
     st.title('My Streamlit App')
     st.write('Welcome to my first Streamlit app!')
     st.write('Here is a DataFrame: nice and simple.')
+    st.write('Here is a DataFrame: nice2 and simple.')
 
 if __name__ == '__main__':
     main()
